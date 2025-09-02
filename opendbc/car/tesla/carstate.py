8e884aa01b89ef3639c52bd44d463f137c75c82d
guii <>
2025-09-03 00:15:35 +0200

    HW2.5 workaround

--- opendbc/car/tesla/carstate.py
+++ opendbc/car/tesla/carstate.py
@@ -108,7 +108,7 @@ class CarState(CarStateBase):
     ret.stockLkas = cp_ap_party.vl["DAS_steeringControl"]["DAS_steeringControlType"] == 2  # LANE_KEEP_ASSIST
 
     # Stock Autosteer should be off (includes FSD)
-    if self.CP.carFingerprint in (CAR.TESLA_MODEL_3, CAR.TESLA_MODEL_Y):
+    if self.CP.carFingerprint in (CAR.TESLA_MODEL_Y):
       ret.invalidLkasSetting = cp_ap_party.vl["DAS_settings"]["DAS_autosteerEnabled"] != 0
     else:
       pass
